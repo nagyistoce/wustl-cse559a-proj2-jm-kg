9bcd9e18debedd8e8d13f9073ad0c8eb199c13fb
kurtglastetter <kurtglastetter@6cf37602-a747-5c1c-d87a-3d32bee17a37>
2010-03-19 06:00:11 +0000

    Renamed "faces" to "subjects", to be more specific

--- trunk/read_face_db.m
+++ trunk/read_face_db.m
@@ -1,15 +1,15 @@
 function face_db = read_face_db()
 
-num_faces = 40;
-num_images_per_face = 10;
+num_subjects = 40;
+num_images_per_subject = 10;
 image_height = 112;
 image_width = 92;
 
-face_db = uint8(zeros(num_faces, num_images_per_face, ...
+face_db = uint8(zeros(num_subjects, num_images_per_subject, ...
                       image_height, image_width));
 
-for ix=1:num_faces
-    for jx=1:num_images_per_face
+for ix=1:num_subjects
+    for jx=1:num_images_per_subject
         face_db(ix, jx, :, :) = imread(strcat('./orl_faces/s', int2str(ix), ...
                                               '/', int2str(jx), '.pgm'));
     end
