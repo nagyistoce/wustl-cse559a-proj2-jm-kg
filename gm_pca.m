8848129b1fa9010fd0c069da2a8c4a73b96c8be3
kurtglastetter <kurtglastetter@6cf37602-a747-5c1c-d87a-3d32bee17a37>
2010-03-20 16:33:18 +0000

    Added num_coeffs parameter to gm_pca.

--- gm_pca.m
+++ gm_pca.m
@@ -1,7 +1,7 @@
 % PCA Face Recognition
 % Kurt Glastetter and Josh Mason
 
-function [fmean U S V] = gm_pca(fmatrix)
+function [fmean U S V] = gm_pca(fmatrix, num_coeffs)
 
 % get mean face (grrr)
 fmean = mean(fmatrix,2);
@@ -9,10 +9,10 @@ fmean = mean(fmatrix,2);
 % subtract mean face from each face
 fmatrix2 = fmatrix - repmat(fmean,1,size(fmatrix,2));
 
-% perform SVD, 30 is number of coefficients to find, and num of basis
+% perform SVD, num_coeffs is number of coefficients to find, and num of basis
 % images to create, this is arbitray...use more for better results
 % D = USV
-[U S V] = svds(fmatrix2,30);
+[U S V] = svds(fmatrix2,num_coeffs);
 
 end
 
