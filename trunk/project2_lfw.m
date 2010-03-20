dbc041f5a644d3327903ce3dde3e4dfa0d5e69d9
kurtglastetter <kurtglastetter@6cf37602-a747-5c1c-d87a-3d32bee17a37>
2010-03-20 22:46:34 +0000

    Added lfw versions of files.  Results are not very good, though.

---
+++ trunk/project2_lfw.m
@@ -0,0 +1,25 @@
+% PCA Face Recognition
+% Kurt Glastetter and Josh Mason
+clear;
+
+%% read in faces
+[fmatrix,names] = get_faces('lfw_faces');
+
+%% perform PCA
+[fmean U S V] = gm_pca(fmatrix,30);
+
+% show mean face
+%meanim = reshape(fmean,130,110);
+%imshow(uint8(meanim));
+
+%% read in test faces
+[tfmatrix,tnames] = get_faces('lfw_test');
+
+%% do recognition and get back recognition matrix
+rmatrix = gm_recognition(tfmatrix, fmean, U, V, tnames, names);
+
+%% calculate results
+[results right wrong] = calc_results(rmatrix);
+
+% dump num right/wrong to screen
+[right wrong]
