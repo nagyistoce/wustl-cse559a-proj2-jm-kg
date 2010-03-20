f8f77c59a24af907fdc6484e803cd4977b44fd9e
kurtglastetter <kurtglastetter@6cf37602-a747-5c1c-d87a-3d32bee17a37>
2010-03-20 22:46:34 +0000

    Added lfw versions of files.  Results are not very good, though.

---
+++ get_faces_lfw.m
@@ -0,0 +1,77 @@
+% PCA Face Recognition
+% Kurt Glastetter and Josh Mason
+
+%% read in face database from disk
+% numimages is used when recursively calling this function, don't pass
+% it as a param.
+function [images,filenames] = get_faces(directory,preprocess)
+numimages = 0;
+images    = [];
+filenames = {};
+
+files = dir(directory);
+sz    = size(files,1);
+
+if (nargin == 1)
+   preprocess = 0;
+end
+
+for i=1 : 1:sz
+    if (files(i).isdir && ~(strcmp(files(i).name,'.') || ...
+            strcmp(files(i).name,'..') || ...
+            strcmp(files(i).name,'.svn')))
+        newdir = sprintf('%s/%s', directory, files(i).name);
+        % recurse into directory and get additional images and add columns
+        % to end
+        [newimages,newfilenames] = get_faces(newdir,preprocess);
+        images = [images,newimages];
+        filenames = [filenames,newfilenames];
+    else
+        if (~isempty(strfind(files(i).name, '.pgm')) || ...
+                ~isempty(strfind(files(i).name, '.jpg')))
+            file = sprintf('%s/%s', directory, files(i).name);
+            numimages = numimages + 1;
+            im = imread(file);
+            %im = imresize(im, 0.5);
+            im = im((125-65+1):(125+65), (125-55+1):(125+55));
+
+            % check if pre-processing needs to occur
+            if (preprocess == 1)
+               % instead of just using an edge map, we overlay the edge
+               % map onto the original image.
+               %em = edge(im,'canny');
+               %im(em) = 255;
+
+               % regular edge detection..
+               %im = edge(im,'canny');
+
+               % gaussian blur
+               %h = fspecial('gaussian',10,10);
+               %im = imfilter(im,h);
+
+               %h = fspecial('log');
+               %im = imfilter(im,h);
+
+               % histogram equalization
+               im = histeq(im);
+
+              %imshow(im)
+              %pause
+            end
+
+            % add new image to new column in images
+            images(:,numimages) = im(:);
+            filenames(numimages) = {file};
+            %imshow(file);
+            %pause
+        end
+    end
+end
+
+end
+
+% test code
+% [images,names] = get_faces('att_faces');
+% im301 = imread(char(names(301)));
+% im301 = im301(:);
+% isequal(im301,images(301)) % better be true
---
+++ project2_mean_faces_lfw.m
@@ -0,0 +1,54 @@
+% Mean Face Recognition
+% Kurt Glastetter and Josh Mason
+
+subjects = keys(get_subjects('lfw_faces'));
+
+mean_faces = zeros(130*110, size(subjects,2));
+
+for ix=1:size(subjects,2)
+
+    %% read in faces
+    subject = subjects(ix);
+    subject = subject{1};
+    [fmatrix,names] = get_faces(sprintf('lfw_faces/%s', subject));
+
+    % get mean face (grrr)
+    fmean = mean(fmatrix,2);
+
+    %% show mean face
+    %meanim = reshape(fmean,112,92);
+    %imshow(uint8(meanim));
+
+    %% save the mean faces for later
+    mean_faces(:, ix) = fmean;
+
+end
+
+%% read in test faces
+[tfmatrix,tnames] = get_faces('lfw_test');
+
+rmatrix = [];
+
+for ix=1:size(tfmatrix,2)
+
+    test_face = tfmatrix(:,ix);
+
+    distancesSquared = sum((mean_faces - ...
+                            repmat(test_face, [1 size(mean_faces,2)])).^2);
+
+    [c index] = min(distancesSquared);
+
+    true_subject = subjects(ix);
+    found_subject = subjects(index);
+
+    true_subject = sprintf('orl_faces/%s/', true_subject{1});
+    found_subject = sprintf('test_faces/%s/', found_subject{1});
+    rmatrix = [rmatrix; {true_subject, found_subject}];
+
+end
+
+%% calculate results
+[results right wrong] = calc_results(rmatrix);
+
+% dump num right/wrong to screen
+[right wrong]
