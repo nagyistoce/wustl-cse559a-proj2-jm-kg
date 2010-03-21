ec713445ecac72a605896eae2514123e55eac86d
kurtglastetter <kurtglastetter@6cf37602-a747-5c1c-d87a-3d32bee17a37>
2010-03-21 03:55:40 +0000

    Added image_adjust_func parameter to get_faces.
    get_faces_lfw calls can now be replaced with get_faces.

--- trunk/get_faces.m
+++ trunk/get_faces.m
@@ -2,7 +2,7 @@
 % Kurt Glastetter and Josh Mason
 
 %% read in face database from disk
-function [images,filenames] = get_faces(directory,preprocess)
+function [images,filenames] = get_faces(directory,image_adjust_func,preprocess)
 numimages = 0;
 images    = [];
 filenames = {};
@@ -11,7 +11,12 @@ files = dir(directory);
 sz    = size(files,1);
 
 if (nargin == 1)
-   preprocess = 0; 
+    image_adjust_func = @(im)(im); % do nothing
+    preprocess = 0;
+end
+
+if (nargin == 2)
+   preprocess = 0;
 end
 
 for i=1 : 1:sz
@@ -21,33 +26,34 @@ for i=1 : 1:sz
         newdir = sprintf('%s/%s', directory, files(i).name);
         % recurse into directory and get additional images and add columns
         % to end
-        [newimages,newfilenames] = get_faces(newdir,preprocess);
+        [newimages,newfilenames] = get_faces(newdir,image_adjust_func,preprocess);
         images = [images,newimages];
         filenames = [filenames,newfilenames];
     else
         if (~isempty(strfind(files(i).name, '.pgm')) || ...
                 ~isempty(strfind(files(i).name, '.jpg')))
             file = sprintf('%s/%s', directory, files(i).name);
-            numimages = numimages + 1;            
+            numimages = numimages + 1;
             im = imread(file);
-            
+            im = image_adjust_func(im);
+
             % check if pre-processing needs to occur
             if (preprocess == 1)
                % instead of just using an edge map, we overlay the edge
                % map onto the original image.
                %em = edge(im,'canny');
                %im(em) = 255;
-               
+
                % regular edge detection..
                %im = edge(im,'canny');
-               
+
                % gaussian blur
                %h = fspecial('gaussian',10,10);
                %im = imfilter(im,h);
-               
+
                %h = fspecial('log');
                %im = imfilter(im,h);
-               
+
                % histogram equalization
                im = histeq(im);
                %im = adapthisteq(im);
@@ -56,7 +62,7 @@ for i=1 : 1:sz
              % imshow(im)
              % pause
             end
-            
+
             % add new image to new column in images
             images(:,numimages) = im(:);
             filenames(numimages) = {file};
