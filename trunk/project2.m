047de4102a64099aab54b3c8a3c7cb151170481f
kurtglastetter <kurtglastetter@6cf37602-a747-5c1c-d87a-3d32bee17a37>
2010-03-21 04:36:30 +0000

    Tried to isolate differences between project2 and project2_lfw.

--- trunk/project2.m
+++ trunk/project2.m
@@ -2,18 +2,24 @@
 % Kurt Glastetter and Josh Mason
 clear;
 
+faces_dir = 'orl_faces';
+test_faces_dir = 'test_faces';
+image_height = 112;
+image_width = 92;
+preprocess_func = @(im)(im); % do nothing
+
 %% read in faces
-[fmatrix,names] = get_faces('orl_faces');
+[fmatrix,names] = get_faces(faces_dir, preprocess_func);
 
 %% perform PCA
 [fmean U S V] = gm_pca(fmatrix,2);
 
 % show mean face
-%meanim = reshape(fmean,112,92);
+%meanim = reshape(fmean,image_height,image_width);
 %imshow(uint8(meanim));
 
 %% read in test faces
-[tfmatrix,tnames] = get_faces('test_faces');
+[tfmatrix,tnames] = get_faces(test_faces_dir, preprocess_func);
 
 %% do recognition and get back recognition matrix
 % the last number specifies the k value for knn
--- trunk/project2_lfw.m
+++ trunk/project2_lfw.m
@@ -2,18 +2,24 @@
 % Kurt Glastetter and Josh Mason
 clear;
 
+faces_dir = 'lfw_faces';
+test_faces_dir = 'lfw_test';
+image_height = 130;
+image_width = 110;
+preprocess_func = @crop_approx_face_rectangle;
+
 %% read in faces
-[fmatrix,names] = get_faces('lfw_faces', @crop_approx_face_rectangle);
+[fmatrix,names] = get_faces(faces_dir, preprocess_func);
 
 %% perform PCA
 [fmean U S V] = gm_pca(fmatrix,2);
 
 % show mean face
-%meanim = reshape(fmean,130,110);
+%meanim = reshape(fmean,image_height,image_width);
 %imshow(uint8(meanim));
 
 %% read in test faces
-[tfmatrix,tnames] = get_faces('lfw_test', @crop_approx_face_rectangle);
+[tfmatrix,tnames] = get_faces(test_faces_dir, preprocess_func);
 
 %% do recognition and get back recognition matrix
 % the last number specifies the k value for knn
